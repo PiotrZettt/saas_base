70f77c96a5535281a967db6295bd9641e93c5a1c
PiotrZettt <piotrzielinski@o2.pl>
2023-06-17 22:34:45 +0100

    setup project && accounts app

---
+++ accounts/utils.py
@@ -0,0 +1,6 @@
+def get_tenants_map():
+    return {
+        "superform.localhost": "superform",
+        "paulspanels.localhost": "paulspanels",
+        "startapphouse.localhost": "startapphouse",
+    }
